65206bd25a5f332896f85e9474e69c2b9785a4a3
Aspect13 <ad13box@gmail.com>
2024-04-18 17:15:42 +0400

    integration create with id fix

--- module.py
+++ module.py
@@ -19,7 +19,7 @@
 from pylon.core.tools import log  # pylint: disable=E0611,E0401
 from pylon.core.tools import module
 
-from tools import constants as c
+from tools import constants as c, db
 
 from ..integrations.models.integration import IntegrationAdmin  # pylint: disable=E0611,E0401
 from .models.integration_pd import IntegrationModel
@@ -94,7 +94,8 @@ class Module(module.ModuleModel):
             "status": "success"
         }
         integration = IntegrationAdmin(**integration_args)
-        integration.insert()
+        with db.get_session() as session:
+            integration.insert(session)
         log.info('Integration created: [id: %s, name: %s]', integration.id, integration.name)
 
     def deinit(self):  # pylint: disable=R0201
